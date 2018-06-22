87117c922394aa18fa1da4b297602546acf9e53b
Dmitry <lbvf50.mobile@gmail.com>
2018-06-22 23:13:52 +0700

    Use Array#product in generator.rb

--- generator.rb
+++ generator.rb
@@ -3,11 +3,9 @@
 def write_lines(file)
     speakers="jane|oksana|alyss|omazh|zahar|ermil"
     emotions="good|neutral|evil"
-    speakers.split("|").each do |name|
-        emotions.split("|").each do |emotion|
-            song ="'Hi, my name is #{name}. In darkened woods the smell of fallen showers all around. Keep in time with may day flowers make love to the ground'" 
-            file.puts "echo #{song} | tts -s #{name} -e #{emotion} > #{name}#{emotion}.mp3" 
-        end    
+    speakers.split("|").product(emotions.split("|")) do |name, emotion|
+        song ="'Hi, my name is #{name}. In darkened woods the smell of fallen showers all around. Keep in time with may day flowers make love to the ground'" 
+            file.puts "echo #{song} | tts -s #{name} -e #{emotion} > #{name}#{emotion}.mp3"
     end
 end
 

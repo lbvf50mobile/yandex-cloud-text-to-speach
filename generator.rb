4ea6d5afd1bd33c3c9ac215f409d565922124f29
Dmitry <lbvf50.mobile@gmail.com>
2018-06-22 22:49:25 +0700

    Generate emotion too

--- generator.rb
+++ generator.rb
@@ -1,5 +1,16 @@
 # Generate sound for different voices
-"jane|oksana|alyss|omazh|zahar|ermil".split("|").each do |x|
-    
-    system("echo 'Hi, my name is #{x}.' | bash tts > #{x}.mp3")
+
+def write_lines(file)
+    speakers="jane|oksana|alyss|omazh|zahar|ermil"
+    emotions="good|neutral|evil"
+    speakers.split("|").each do |name|
+        emotions.split("|").each do |emotion|
+            song ="'Hi, my name is #{name}. In darkened woods the smell of fallen showers all around. Keep in time with may day flowers make love to the ground'" 
+            file.puts "echo #{song} | tts -s #{name} -e #{emotion} > #{name}#{emotion}.mp3" 
+        end    
+    end
+end
+
+File.open('generator.sh','w') do |f|
+    write_lines f
 end

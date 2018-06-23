c0a8ffa8f3735eb2b034a0387071268e31d96085
Dmitry <lbvf50.mobile@gmail.com>
2018-06-24 02:48:39 +0700

    Ubuntu 16.04 tts from from Ruby script, Windows 8.1. /dev/stdout Devise or resource busy

--- runner.rb
+++ runner.rb
@@ -1,10 +1,10 @@
 # Try to run pipe from Ruby script:
 # https://stackoverflow.com/a/2400/8574922
 p "1) backquites"
-`echo 'Hi' | bash tts  > test1.mp3` # tts: line 73: /dev/stdout: Device or resource busy
+`echo 'Hi' | bash tts  > test1.mp3` # Ubuntu 16.04 - works; Windows 8.1 - error tts: line 73: /dev/stdout: Device or resource busy
 p "2) %x"
-%x{echo 'Hi' | bash tts  > test2.mp3} # tts: line 73: /dev/stdout: Device or resource busy
+%x{echo 'Hi' | bash tts  > test2.mp3} # Ubuntu 16.04 - works; Windows 8.1 - error tts: line 73: /dev/stdout: Device or resource busy
 p "3) system"
-system("echo 'Hi' | bash tts  > test3.mp3") # tts: line 73: /dev/stdout: Device or resource busy
+system("echo 'Hi' | bash tts  > test3.mp3") # Ubuntu 16.04 - works; Windows 8.1 - error tts: line 73: /dev/stdout: Device or resource busy
 p "4) Exec"
-exec("echo 'Hi' | bash tts  > test4.mp3") # tts: line 73: /dev/stdout: Device or resource busy
+exec("echo 'Hi' | bash tts  > test4.mp3") # Ubuntu 16.04 - works; Windows 8.1 - error tts: line 73: /dev/stdout: Device or resource busy

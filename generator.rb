d5000da7320ba9fb3f4e7297bc2fc8264a075652
Dmitry <lbvf50.mobile@gmail.com>
2018-06-24 14:35:15 +0700

    BUG in parsing

--- generator.rb
+++ generator.rb
@@ -3,10 +3,11 @@
 def write_lines(file)
     speakers="jane" # |oksana|alyss|omazh|zahar|ermil"
     emotions="good" # |neutral|evil"
-    speeds="1.0|2.0|3.0"
+    speeds="1.0" # |2.0|3.0"
     speakers.split("|").product(emotions.split("|")).to_a.product(speeds.split("|")) do |(name, emotion), speed|
         song ="'Hi, my name is #{name}. In darkened woods the smell of fallen showers all around. Keep in time with may day flowers make love to the ground'" 
-            file.puts "echo #{song} | tts -s #{name} -e #{emotion} -S #{speed} > #{name}#{emotion}#{speed}.mp3"
+        file.puts "echo #{song} | tts -s #{name} -e #{emotion} -S #{speed} > #{name}#{emotion}#{speed}_std.mp3"
+        file.puts "tts -s #{name} -e #{emotion} -S #{speed} #{song}  #{name}#{emotion}#{speed}_args.mp3"
     end
 end
 
--- generator.sh
+++ generator.sh
@@ -1,3 +1,2 @@
-echo 'Hi, my name is jane. In darkened woods the smell of fallen showers all around. Keep in time with may day flowers make love to the ground' | tts -s jane -e good -S 1.0 > janegood1.0.mp3
-echo 'Hi, my name is jane. In darkened woods the smell of fallen showers all around. Keep in time with may day flowers make love to the ground' | tts -s jane -e good -S 2.0 > janegood2.0.mp3
-echo 'Hi, my name is jane. In darkened woods the smell of fallen showers all around. Keep in time with may day flowers make love to the ground' | tts -s jane -e good -S 3.0 > janegood3.0.mp3
+echo 'Hi, my name is jane. In darkened woods the smell of fallen showers all around. Keep in time with may day flowers make love to the ground' | tts -s jane -e good -S 1.0 > janegood1.0_std.mp3
+tts -s jane -e good -S 1.0 'Hi, my name is jane. In darkened woods the smell of fallen showers all around. Keep in time with may day flowers make love to the ground'  janegood1.0_args.mp3
